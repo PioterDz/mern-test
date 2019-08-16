bc80a7c18716e3c4ec1ed48fee991d3299c3b199
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-08-16 19:47:40 +0200

    connect to mongo cluster

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: 8000,
-    DB: 'mongodb://localhost:27017/mernapp',
+    DB: 'mongodb+srv://pioter:mongomongo@cluster0-11ycm.gcp.mongodb.net/test?retryWrites=true&w=majority',
 };
