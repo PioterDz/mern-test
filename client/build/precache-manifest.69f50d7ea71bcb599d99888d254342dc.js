cc9beac5922e3e2eb7eacfd7632290584d1c71a3
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-08-23 13:43:29 +0200

    remove build from gitignore

---
+++ client/build/precache-manifest.69f50d7ea71bcb599d99888d254342dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f486f8f75e32631cee10869ff4b4bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0299995e50186cd60169",
+    "url": "/static/css/2.140d160d.chunk.css"
+  },
+  {
+    "revision": "92e423d6e517c08b3139",
+    "url": "/static/css/main.847b5087.chunk.css"
+  },
+  {
+    "revision": "0299995e50186cd60169",
+    "url": "/static/js/2.1d34afc7.chunk.js"
+  },
+  {
+    "revision": "92e423d6e517c08b3139",
+    "url": "/static/js/main.a9aaa750.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
