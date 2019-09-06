1690fbc7fddf6ce266eb0a6d06351bafb50323e7
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-09-06 16:31:25 +0200

    dif names for heroku env

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API;
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const API_URL = process.env.REACT_APP_API || process.env.DB_API;
+export const BASE_URL = process.env.REACT_APP_BASE_URL || process.env.DB_BASE;
